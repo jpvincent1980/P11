6f2b55b3a95cf1104ba14a31c91022e47c5f798a
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2021-11-16 14:47:39 +0100

    Fix unknown email bug

--- server.py
+++ server.py
@@ -24,10 +24,22 @@ clubs = loadClubs()
 def index():
     return render_template('index.html')
 
+
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    """
+    Route to index page when users can login.
+
+    :return: a template
+    """
+    try:
+        club = [club for club in clubs if club['email'] == request.form['email']][0]
+        return render_template('welcome.html',club=club,competitions=competitions)
+    except IndexError:
+        message = "Sorry, that email wasn't found."
+        flash(message, 'error')
+        return redirect(url_for('index'))
+
 
 
 @app.route('/book/<competition>/<club>')
