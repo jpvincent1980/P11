1da6c4b5e697b39c5b80785a297cfd63a4622fe9
Jean-Philippe Vincent <jpvincent@hotmail.fr>
2021-11-22 11:11:13 +0100

    Locust config file added + locustfile.py updated

--- tests/tests_performance/locustfile.py
+++ tests/tests_performance/locustfile.py
@@ -1,8 +1,35 @@
 from locust import HttpUser, task
 
+EMAIL = "john@simplylift.co"
+COMPETITION = "Spring Festival"
+CLUB = "Simply Lift"
+PLACES = "2"
+
 
 class ServerTest(HttpUser):
 
     @task
     def login(self):
         self.client.get("/")
+
+    @task
+    def showSummary(self):
+        self.client.post('/showSummary', data=dict(email=EMAIL))
+
+    @task
+    def book(self):
+        self.client.get('/book/' + COMPETITION + '/' + CLUB)
+
+    @task
+    def purchasePlaces(self):
+        self.client.post('/purchasePlaces', data=dict(club=CLUB,
+                                                      competition=COMPETITION,
+                                                      places=PLACES))
+
+    @task
+    def displayClubsPoints(self):
+        self.client.get('/displayClubsPoints')
+
+    @task
+    def logout(self):
+        self.client.get('/logout')
